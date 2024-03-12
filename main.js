3be26e8513ab1f77afe81bedc14ba7ae492442ae
superdev808 <drose6307@gmail.com>
2024-03-12 07:37:03 -0500

    refactored code

--- main.js
+++ main.js
@@ -1,75 +1,59 @@
-const fs = require('fs');
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-const path = require('path');
-const xlsx = require('xlsx');
-const dayjs = require('dayjs');
+const fs = require("fs");
+const recursive = require("recursive-readdir");
+const makeDir = require("make-dir");
+const path = require("path");
+const xlsx = require("xlsx");
 
-function askQuestion(query) {
-    return new Promise((resolve) => readline.question(query, resolve));
-}
+require("dotenv").config();
 
 function readFile(filePath) {
     const resolvedPath = path.resolve(filePath);
     const fileExtension = path.extname(resolvedPath).toLowerCase();
 
     switch (fileExtension) {
-        case '.csv':
+        case ".csv":
             return parseCsvToJSON(resolvedPath);
-        case '.xlsx':
-        case '.xls':
+        case ".xlsx":
+        case ".xls":
             return parseExcelToJSON(resolvedPath);
         default:
-            console.log('Unsupported file format');
+            console.log("Unsupported file format");
             return [];
     }
 }
 
-function parseCsvToJSON(filePath) {
+function isNotExcelFile(filePath) {
+    return (
+        [".csv", ".xlsx", ".xls"].indexOf(
+            path.extname(filePath).toLowerCase()
+        ) < 0
+    );
+}
+
+function parseExcelToJSON(filePath) {
     const workbook = xlsx.readFile(filePath);
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
     return xlsx.utils.sheet_to_json(worksheet, { defval: "" });
 }
 
-function parseExcelToJSON(filePath) {
-    return parseCsvToJSON(filePath); // xlsx.readFile works for both Excel and CSV formats
-}
-
 async function main() {
-    const filePaths = [
-        "Bone Reduction Instruments - All Brands",
-        "Chairside Pick-Up Materials - All Brands",
-        "Drill Kit and Drill Sequences - All Brands",
-        "Drivers (Restorative, Multi-Unit Abutments) - All Brands",
-        "Healing Abutments - All Brands ",
-        "Implant Analogs - All Brands",
-        "Implant Drivers (Restorative, Direct to Implant) - All Brands",
-        "Implant Screws - All Brands",
-        "Implants - All Brands ",
-        "Impression Copings (Direct to Implant) - All Brands ",
-        "Impression Copings (MUAs) - All Brands ",
-        "MUAs - All Brands ",
-        "Scanbodies (Multi-Unit Abutments) - All Brands",
-        "Scanbodies (Single Unit) - All Brands",
-        "Scanbody Drivers (Direct to Implant) - All Brands",
-        "Scanbody Drivers (MUAs) - All Brands ",
-        "Stock Abutments - All Brands ",
-        "Temporary Copings (Direct to Implant) All Brands",
-        "Temporary Copings (MUAs) - All Brands",
-        "Ti Base Calculator (Direct to Implant) - All Brands"
-    ]
-    // const filePath = await askQuestion('Input the path to the file (.csv, .xlsx, .xls): ');
-    // readline.close();
-    // filePaths.forEach((filePath) => {
-        const jsonData = readFile(`data/Bone Reduction Instruments - All Brands.xlsx`);
-        const outputPath = `output/Bone Reduction Instruments - All Brands.json`;
-        fs.writeFileSync(outputPath, JSON.stringify(jsonData, null, 2));
-        console.info(`Result is saved in ${outputPath}`);
-        readline.close();
-    // })
+    const filePaths = await recursive(process.env.XLSX_DIR, [isNotExcelFile]);
+    await makeDir(process.env.JSON_DIR);
+    console.log(filePaths);
+    filePaths.forEach((filePath) => {
+        parseExcelToJSON(filePath);
+        const fileName = path.basename(filePath);
+        const outputPath = path.join(
+            process.env.JSON_DIR,
+            fileName.slice(0, fileName.lastIndexOf(".")) + ".json"
+        );
+        fs.writeFileSync(
+            outputPath,
+            JSON.stringify(parseExcelToJSON(filePath), null, 2)
+        );
+        console.log(`${fileName} writing done`);
+    });
 }
 
 main().catch(console.error);
